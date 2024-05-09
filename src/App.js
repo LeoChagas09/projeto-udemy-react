a51dc6130e78d4acdaab7453eeb03f399964cd8a
LeoChagas09 <leo_kikinhoo@hotmail.com>
2024-05-09 20:10:56 -0300

    Iniciando projeto react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+function App(){
+
+  const [input, setInputs] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Ir para o Google segunda',
+    'Estudar React'
+  ]);
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('@tarefas');
+
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  
+
+  function handleRegister(e){
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInputs('');
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa:</label>
+        <input placeholder="Digite uma tarefa" type="text" value={input}  onChange={(e) => setInputs(e.target.value)}/>
+
+        <br/>
+        <br/>
+
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br />
+
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      
     </div>
-  );
+  )
 }
 
 export default App;
