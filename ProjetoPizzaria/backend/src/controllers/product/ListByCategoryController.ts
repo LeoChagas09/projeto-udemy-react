810d300a925cbe1cd25e47b9a2396b1e1ccc9e53
LeoChagas09 <leochagas09@hotmail.com>
2024-05-15 16:31:15 -0300

    iniciando backend, criacao de usuario, categorias e produtos

---
+++ ProjetoPizzaria/backend/src/controllers/product/ListByCategoryController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ListByCategoryService } from '../../services/product/ListByCategoryService';
+
+
+class ListByCategoryController {
+  async handle(req: Request, res: Response) {
+    const category_id  = req.query.category_id as string;
+
+    const listByCategoryService = new ListByCategoryService();
+
+    const category = await listByCategoryService.execute({category_id});
+
+    return res.json(category);
+  }
+}
+
+export { ListByCategoryController };
---
+++ ProjetoPizzaria/backend/src/routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import multer from 'multer';
+import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
+
+import uploadConfig from './config/multer';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+
+const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
+
+// Routas USER
+router.post('/users', new CreateUserController().handle);
+
+router.post('/session', new AuthUserController().handle);
+
+router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+// Routas CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+
+// Routas PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
+
+export { router };
