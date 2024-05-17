1a6fa603d2d03b19994481f2b875a45973d05f64
LeoChagas09 <leochagas09@hotmail.com>
2024-05-17 10:50:12 -0300

    Finalizando o backend do projeto pizzaria

--- ProjetoPizzaria/backend/src/routes.ts
+++ ProjetoPizzaria/backend/src/routes.ts
@@ -10,25 +10,45 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 
 import uploadConfig from './config/multer';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrdersController } from './controllers/order/ListOrdersController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp"));
 
-// Routas USER
+// Rotas USER
 router.post('/users', new CreateUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
 
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 
-// Routas CATEGORY
+// Rotas CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 
-// Routas PRODUCT
+// Rotas PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
+
+// Rotas Order
+router.post('/order', isAuthenticated, new CreateOrderController().handle);
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+
+// Rotas Items
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
+router.put('/order/send', isAuthenticated, new SendOrderController().handle);
+router.get('/orders', isAuthenticated, new ListOrdersController().handle);
+router.get('/orders/detail', isAuthenticated, new DetailOrderController().handle);
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle);
 
 export { router };
